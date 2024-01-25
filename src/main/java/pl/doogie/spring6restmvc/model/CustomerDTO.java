30e24ea2f93f3bd080ad04b70be3792bf524231b
doooooogie <bartosz@dlugosz.org.pl>
2024-01-25 09:03:17 +0100

    JPA

--- src/main/java/pl/doogie/spring6restmvc/model/CustomerDTO.java
+++ src/main/java/pl/doogie/spring6restmvc/model/CustomerDTO.java
@@ -1,28 +1,17 @@
 package pl.doogie.spring6restmvc.model;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
+import lombok.Builder;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Getter
-@Setter
 @Builder
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
+@Data
 public class CustomerDTO {
 
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
     private UUID id;
     private String name;
-
-    @Version
     private Integer version;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
